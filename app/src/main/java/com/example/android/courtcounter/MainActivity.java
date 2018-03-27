2639ab93b7e733511762d960900d431b7062b9ed
Jacqueline T <awmawgawd@users.noreply.github.com>
2018-03-26 21:44:13 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.android.courtcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void plusThree(View view){
+        displayForTeamA(3);
+    }
+
+    public void plusTwo(View view){
+        displayForTeamA(2);
+    }
+
+    public void freethrow(View view){
+        displayForTeamA(1);
+    }
+}
